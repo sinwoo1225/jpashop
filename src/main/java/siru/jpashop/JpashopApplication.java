e211c34f3cc5632e0f762aaebedfb29c877e5ab6
shinwoo <sinwoo1225@gmail.com>
2021-10-09 22:30:42 +0900

    Initial commit

---
+++ src/main/java/siru/jpashop/JpashopApplication.java
@@ -0,0 +1,13 @@
+package siru.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/siru/jpashop/repository/MemberRepositoryTest.java
@@ -0,0 +1,42 @@
+package siru.jpashop.repository;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+import siru.jpashop.entity.Member;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    public void testMember() throws Exception {
+        // given
+        Member member = new Member();
+        member.setUsername("siru");
+
+        // when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        // then
+        Assertions.assertEquals(findMember.getId(), member.getId());
+        Assertions.assertEquals(findMember.getUsername(), member.getUsername());
+    }
+
+    @Test
+    void find() {
+
+    }
+
+
+}
