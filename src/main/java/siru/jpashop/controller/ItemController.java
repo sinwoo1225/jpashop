a27338b6758bbd885e771e5202859e82904f5b84
shinwoo <sinwoo1225@gmail.com>
2021-10-19 21:43:23 +0900

    타임리프, css 파일추가, 맴버 등록&조회, 상품 등록 조회 페이지 개발

---
+++ src/main/java/siru/jpashop/controller/ItemController.java
@@ -0,0 +1,45 @@
+package siru.jpashop.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import siru.jpashop.domain.item.Book;
+import siru.jpashop.domain.item.Item;
+import siru.jpashop.service.ItemService;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm bookForm) {
+        Book book = new Book();
+        book.setName(bookForm.getName());
+        book.setPrice(bookForm.getPrice());
+        book.setStockQuantity(bookForm.getStockQuantity());
+        book.setAuthor(bookForm.getAuthor());
+        book.setIsbn(bookForm.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
+
+    @GetMapping("/items")
+    public String list(Model model) {
+        List<Item> itemList = itemService.findItems();
+        model.addAttribute("items", itemList);
+        return "items/itemList";
+    }
+}
