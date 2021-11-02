388f016cc09544c836ce592bf41381a4b3730f88
shinwoo <sinwoo1225@gmail.com>
2021-11-02 12:59:08 +0900

    주문조회 v3 api 추가
    - 패치조인 적용

--- src/main/java/siru/jpashop/controller/OrderSimpleApiController.java
+++ src/main/java/siru/jpashop/controller/OrderSimpleApiController.java
@@ -48,6 +48,17 @@ public class OrderSimpleApiController {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * 패치조인 적용
+     */
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        return orders.stream()
+                .map(SimpleOrderDto::new)
+                .collect(Collectors.toList());
+    }
+
     @Data
     static class SimpleOrderDto {
         private Long orderId;
